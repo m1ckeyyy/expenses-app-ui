799a48cb34a3bc64c3b16eb5894e5c625f8613d7
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2023-08-17 22:56:20 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// import { MDCChipSet } from "@material/chips";
+// const firstButton = document.querySelector(".foo-button");
+// mdc.ripple.MDCRipple.attachTo(firstButton);
+
+// const chipset = new MDCChipSet(
+// 	document.querySelector(".mdc-evolution-chip-set")
+// );
+// console.log(chipset);
