ef830c272cb9a9f0b148290a051515a2809a8c52
m1ckeyyy <90149242+m1ckeyyy@users.noreply.github.com>
2023-08-19 13:49:52 +0000

    layout changes, added image and summary

--- index.js
+++ index.js
@@ -6,3 +6,18 @@
 // 	document.querySelector(".mdc-evolution-chip-set")
 // );
 // console.log(chipset);
+// import { MDCFormField } from "@material/form-field";
+// import { MDCCheckbox } from "@material/checkbox";
+
+// const checkbox = new MDCCheckbox(document.querySelector(".mdc-checkbox"));
+// const formField = new MDCFormField(document.querySelector(".mdc-form-field"));
+// formField.input = checkbox;
+const checkbox = document.querySelector("#checkbox-1");
+checkbox.onclick = function () {
+  console.log(checkbox.checked);
+};
+if (checkbox.checked) {
+  console.log("Checkbox is checked");
+} else {
+  console.log("Checkbox is not checked");
+}
